ac28dc647db9a1464f412a9967096d4787ce2699
vankata6 <ikkolev95@gmail.com>
2023-06-29 12:12:51 +0300

    load more button on collection page

---
+++ assets/collection-load-more.js
@@ -0,0 +1,20 @@
+var products_on_page = $('.products-on-page');
+var next_url = products_on_page.data('next-url');
+
+function loadMoreProducts() {
+    $.ajax (
+        {
+            url: next_url,
+            type: 'GET',
+            dataType: 'html'
+        }
+    ).done(function(next_page) {
+        var new_products = $(next_page).find('.products-on-page');
+        var new_url = new_products.data('next-url');
+
+        next_url = new_url;
+
+        products_on_page.append(new_products.html());
+
+    })
+}
